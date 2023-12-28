825be14c2e5c5f5d47e98656c2d7e704087d47ae
owusuvictorjnr <owusuvictor80@gmail.com>
2023-12-28 14:49:40 +0000

    navbar items

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -1,7 +1,74 @@
-import React from 'react'
+'use client'
 
+import React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const NavLinks = [
+  {
+    id: '1',
+    name: 'home',
+    link: '/',
+  },
+
+  {
+    id: '2',
+    name: 'cars',
+    link: '/cars',
+  },
+
+  {
+    id: '3',
+    name: 'about',
+    link: '/about',
+  },
+
+  {
+    id: '4',
+    name: 'contact',
+    link: '/contact',
+  },
+
+  {
+    id: '5',
+    name: 'booking',
+    link: '/booking',
+  },
+]
 const Navbar = () => {
-  return <div>Navbar</div>
+  const pathname = usePathname()
+  return (
+    <>
+      <nav className="shadow-sm">
+        <div className="container">
+          <div className="flex justify-between items-center">
+            <h1 className="text-3xl font-bold capitalize">bossu car rental</h1>
+
+            <div className="hidden md:block">
+              <ul className="flex items-center gap-8">
+                {NavLinks.map((data) => {
+                  const isActive = pathname === data.link
+                  return (
+                    <li
+                      key={data.link}
+                      className={`${isActive && 'text-blue-500'} capitalize`}
+                    >
+                      <Link
+                        href={data.link}
+                        className="inline-block py-2 hover:border-b-2 hover:text-amber-400"
+                      >
+                        {data.name}
+                      </Link>
+                    </li>
+                  )
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
 }
 
 export default Navbar
