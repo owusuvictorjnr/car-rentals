eaaa041a2e16517c766ea61c4f0d70becb6c9266
owusuvictorjnr <owusuvictor80@gmail.com>
2023-12-29 15:17:25 +0000

    initiate the aos package

--- app/(root)/layout.jsx
+++ app/(root)/layout.jsx
@@ -3,6 +3,9 @@
 import Hero from '@/components/Hero/Hero'
 import Navbar from '@/components/shared/Navbar'
 import React, { useEffect, useState } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+// import Cars from '@/app/(root)/cars/page'
 
 export default function RootLayout({ children }) {
   const handleThemeToggle = () => {
@@ -21,10 +24,21 @@ export default function RootLayout({ children }) {
       localStorage.setItem('theme', 'light')
     }
   }, [element.classList, theme])
+
+  // AOS Init
+  React.useEffect(() => {
+    AOS.init({
+      offsets: 100,
+      durations: 800,
+      easing: 'ease-in-sine',
+      delay: 100,
+    })
+    AOS.refresh()
+  })
   return (
     <div className="">
       <Navbar theme={theme} setTheme={handleThemeToggle} />
-      <Hero/>
+      <Hero theme={theme} />
       <main className="">{children}</main>
     </div>
   )
