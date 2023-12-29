cdf41ec9813dda4a9994b607cac4ff847e72b51a
owusuvictorjnr <owusuvictor80@gmail.com>
2023-12-29 18:29:48 +0000

    services

--- app/(root)/service/page.jsx
+++ app/(root)/service/page.jsx
@@ -1,22 +1,9 @@
+'use client'
+
 import React from 'react'
 
 const Services = () => {
-  return (
-    <div className="py-14 dark:bg-black dark:text-white sm:min-h-[40rem] sm:grid sm:place-items-center">
-      <div className="container">
-        <div className="pb-12">
-          <h1 className="text-3xl font-semibold text-center sm:text-4xl">
-            why choose us
-          </h1>
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-            {
-                
-            }
-        </div>
-      </div>
-    </div>
-  )
+  return <div className="">service</div>
 }
 
 export default Services
