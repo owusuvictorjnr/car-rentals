6e947b45c0b6ef8a4222a0b886cb83da1dff9391
owusuvictorjnr <owusuvictor80@gmail.com>
2023-12-31 19:41:26 +0000

    user comment picture initialization

--- next.config.js
+++ next.config.js
@@ -2,3 +2,15 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      // Add other external domains as needed
+    ],
+  },
+}
